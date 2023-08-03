cf94ea6f3592257b3a17387cebd7184d9c20a175
Xiaok <xiaok@zxkxz.cn>
2023-08-03 02:30:40 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # FLY-Tools
+
+## Install
+
+Note: FLY-Tools can automatically obtain the klipper directory through klipper.service in the system. If the automatic acquisition fails, please manually specify the klipper directory.
+
+1. Install FLY-Tools using the install script
+
+    ```
+    wget -O flytools_install.sh https://github.com/kluoyun/FLY-Tools/blob/main/scripts/install.sh && sudo bash flytools_install.sh
+    ```
+
+      
+2. Custom installation
+
+    * You can also manually download a program that matches the system architecture to run.
+    * Use the `-h` parameter to see how to use it
+
+        ```
+        Usage of Fly-Tools_linux_arm64:
+            -c string
+                    Canboot repository directory
+            -k string
+                    Klipper repository directory
+            -l string
+                    Save log output to file
+            -p int
+                    FLY-Tools run WEB port (default 9999)
+            -v    cat FLY-Tools Version
+        ```
+
+3. Open from the browser
+
+    * Open default port 9999
+    * `http://{your device ip}:9999/`
---
+++ scripts/install.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+
+if [ "${SUDO_USER}" = "root" ]; then
+    echo "Please do not execute it under the root user"
+    exit 1
+fi
+
+if [ -z "${SUDO_USER}" ]; then
+    echo "Requires \"sudo \" privileges"
+    exit 1
+fi
+
+[ -n "${BASE_USER}" ] || BASE_USER="${SUDO_USER}"
+
+M=$(uname -m)
+
+URL="https://github.com/kluoyun/FLY-Tools/blob/main/application/Fly-Tools_linux_arm64"
+
+if [ "$M" = "armhf" ] || [ "$M" = "arm" ] ;then
+    URL="https://github.com/kluoyun/FLY-Tools/blob/main/application/Fly-Tools_linux_arm"
+elif [ "$M" = "arm64" ] || [ "$M" = "aarch64" ] ;then
+    URL="https://github.com/kluoyun/FLY-Tools/blob/main/application/Fly-Tools_linux_arm64"
+elif [ "$M" = "x86_64" ];then
+    URL="https://github.com/kluoyun/FLY-Tools/blob/main/application/Fly-Tools_linux_amd64"
+else
+    echo "Unsupported System Architectures: ${M} !!!"
+    exit 1
+fi
+
+sudo apt update
+sudo apt install -y ttyd dfu-util
+
+sudo cat > /etc/default/ttyd << EOF
+# /etc/default/ttyd
+
+TTYD_OPTIONS="-p 9998 -6 -O login"
+EOF
+
+sudo systemctl restart ttyd
+
+sudo systemctl stop FLY-Tools.service
+
+sudo rm /usr/local/bin/FLY-Tools
+sudo wget $URL -O /usr/local/bin/FLY-Tools > /dev/null 2>&1
+sudo chmod +x /usr/local/bin/FLY-Tools
+
+sudo cat > /etc/systemd/system/FLY-Tools.service << EOF
+[Unit]
+Description=FLY-Tools
+Requires=network-online.target
+After=network-online.target
+
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+Type=simple
+Restart=always
+RemainAfterExit=yes
+RestartSec=1
+User=root
+ExecStart=/usr/local/bin/FLY-Tools -p 9999 -l /tmp/FLY-Tools.log
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl restart FLY-Tools.service
+
+echo "安装结束"
